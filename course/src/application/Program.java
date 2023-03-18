e8cf3a99d8707a6b2b3b797c959870267cfd0c41
Vitorchx <vitorchx@outlook.com>
2023-03-17 22:56:46 -0300

    att  17/03

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -18,6 +18,26 @@ public class Program {
 		System.out.println("Quantity in stock");
 		product.quantity = scanner.nextInt();
 
+		System.out.println();
+		System.out.println("Product data: " + product);
+
+		System.out.println();
+		System.out.println("entre com o numero de produtos que sera adicionado em estoque: ");
+		int quantity = scanner.nextInt();
+		product.addProducts(quantity);
+
+		System.out.println();
+		System.out.println("Update data: " + product);
+
+		System.out.println();
+		System.out.println("entre com o numero de produtos que sera removido do estoque: ");
+		quantity = scanner.nextInt();
+		product.removeProducts(quantity);
+
+		System.out.println();
+		System.out.println("Update data: " + product);
+
+		scanner.close();
 	}
 
 }
--- course/src/entities/Product.java
+++ course/src/entities/Product.java
@@ -14,8 +14,13 @@ public class Product {
 		this.quantity += quantity;
 
 	}
-	
+
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
 	}
 }
